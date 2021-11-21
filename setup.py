b324f39e5728d8210ba9faa6514a255fa8650eda
Ashok <test@test.com>
2021-11-21 17:58:22 +0400

    first commit wafer full

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="WaferFault Detection",
+    version="0.0.1",
+    description="ML project",
+    author="Ashok",
+    packages=find_packages(),
+    license="MIT"
+)
